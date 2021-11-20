38c8ae847b4e04a9152b6db2a49be53f88fcb5d8
jetson <jetson@nano-4gb-jp451.localdomain>
2021-11-20 10:58:27 -0800

    changed float to twist

--- ai_race/teleop_gamepad.py
+++ ai_race/teleop_gamepad.py
@@ -2,7 +2,7 @@ import os
 import rclpy
 import pygame
 from rclpy.node import Node
-from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.init()
@@ -14,8 +14,7 @@ class TeleopGamepad(Node):
     
     def __init__(self):
         super().__init__('teleop_gamepad')
-        self.pub_throttle = self.create_publisher(Float32, 'throttle', 10)
-        self.pub_steering = self.create_publisher(Float32, 'steering', 10)
+        self.pub_velocity = self.create_publisher(Twist, 'velocity', 10)
         
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -23,16 +22,15 @@ class TeleopGamepad(Node):
     def timer_callback(self):
         pygame.event.pump()
         
-        throttle = Float32()
-        throttle.data = round(j.get_axis(1), 2) #Left thumbstick Y
-        steering = Float32()
-        steering.data = round(j.get_axis(2), 2) #Right thumbstick X        
         
-        print("Throttle:", throttle.data)
-        print("Steering:", steering.data) 
+        velocity = Twist()
+        velocity.linear.x = round(j.get_axis(1), 2) #Left thumbstick Y
+        velocity.linear.y = round(j.get_axis(2), 2) #Right thumbstick X     
         
-        self.pub_throttle.publish(throttle)
-        self.pub_steering.publish(steering)
+        print("Throttle:", velocity.linear.x)
+        print("Steering:", velocity.linear.y) 
+        
+        self.pub_velocity.publish(velocity)
         
         
 def main(args=None):
