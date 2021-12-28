a61a60cd8281b7dd5bc15cf8a8cbfc056a74ee3d
jetson <jetson@nano-4gb-jp451.localdomain>
2021-12-28 04:57:02 -0800

    edit setup

--- jetson_setup.sh
+++ jetson_setup.sh
@@ -2,12 +2,16 @@ echo "[INFO] entering setup"
 echo "[START] configure power mode"
 sudo nvpmodel -m1
 echo "[FINISHED] configure power mode"
+echo "[START] update & upgrade"
+sudo apt-get update
+echo "[FINISHED] update & upgrade"
 echo "[START] install v4l-utils"
 sudo apt install -y v4l-utils
 echo "[FINISHED] install v4l-utils"
 echo "[START] downgrade docker"
 sudo apt install -y containerd=1.5.2-0ubuntu1~18.04.3
 sudo apt install -y docker.io=19.03.6-0ubuntu1~18.04.3
+sudo apt-mark hold docker.io containerd
 echo "[FINISHED] downgrade docker"
 echo "[START] download docker base image"
 sudo docker pull dustynv/ros:foxy-ros-base-l4t-r32.5.0
