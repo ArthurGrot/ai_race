7207d22d4473230537b66a87b63f7ee7af2662dd
jetson <jetson@nano-4gb-jp451.localdomain>
2022-03-27 06:33:18 -0700

    namespaces

--- launch/jetbot_1.launch.py
+++ launch/jetbot_1.launch.py
@@ -5,39 +5,32 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='ai_race',
-            namespace='ai_race',
+            namespace='ai_race_jetbot_1',
             executable='teleop_gamepad',
             name='ai_race_teleop'
         ),
         Node(
             package='ai_race',
-            namespace='ai_race',
-            executable='motor_jetracer',
+            namespace='ai_race_jetbot_1',
+            executable='motor_jetbot',
             name='ai_race_motor'
         ),
         Node(
             package='ai_race',
-            namespace='ai_race',
+            namespace='ai_race_jetbot_1',
             executable='camera_pub',
             name='ai_race_camera_pub'
         ),
         Node(
             package='ai_race',
-            namespace='ai_race',
+            namespace='ai_race_jetbot_1',
             executable='flask_server',
             name='ai_race_flask_server'
         ),
         Node(
             package='ai_race',
-            namespace='ai_race',
+            namespace='ai_race_jetbot_1',
             executable='line_follower',
             name='ai_race_line_follower'
         )
-        # ,
-        # Node(
-        #     package='ai_race',
-        #     namespace='ai_race',
-        #     executable='display',
-        #     name='ai_race_display'
-        # )
     ])
