0d884a00c4f145841ad60ef502533ac3fb90a782
jetson <jetson@nano-4gb-jp451.localdomain>
2022-04-09 11:50:17 -0700

    Now 5 frames buffer for yolo

--- ai_race/yolo_jetracer.py
+++ ai_race/yolo_jetracer.py
@@ -37,7 +37,8 @@ class ImageSubscriberYolo(Node):
         super().__init__('image_subscriber_yolo')
         # '/workspace/src/ai_race/ai_race/models/road_following_model30a.pth'
         
-        self.model = torch.hub.load('/workspace/src/ai_race/ai_race/yolov5/', 'custom', path='/workspace/src/ai_race/ai_race/models/yolov5Detection.pt', source='local', device='cpu') # local repo
+        self.model = torch.hub.load('/workspace/src/ai_race/ai_race/yolov5/', 'custom', path='/workspace/src/ai_race/ai_race/models/yolov5Detection.pt', source='local') # local repo
+        self.model.cuda()
         
         # parameters for distance estimation
         self.focal_length = 0.315
@@ -63,7 +64,7 @@ class ImageSubscriberYolo(Node):
             Image,
             'video_frames_480',
             self.listener_callback,
-            1)
+            5)
         self.subscription
         #self.i = 0
         self.br = CvBridge()
