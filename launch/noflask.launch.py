04092979b262db82d2da74cc8aa22ead7577c1f3
jetson <jetson@nano-4gb-jp451.localdomain>
2022-05-09 03:34:09 -0700

    noflask launch file

---
+++ launch/noflask.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ai_race',
+            namespace='ai_race_jetracer_1',
+            executable='camera_pub',
+            name='ai_race_camera_pub'
+        ),
+        Node(
+            package='ai_race',
+            namespace='ai_race_jetracer_1',
+            executable='teleop_gamepad',
+            name='ai_race_teleop'
+        ),
+        Node(
+            package='ai_race',
+            namespace='ai_race_jetracer_1',
+            executable='motor_jetracer',
+            name='ai_race_motor'
+        ),
+        Node(
+            package='ai_race',
+            namespace='ai_race_jetracer_1',
+            executable='line_follower',
+            name='ai_race_line_follower'
+        ),
+        Node(
+            package='ai_race',
+            namespace='ai_race_jetracer_1',
+            executable='motor_processing',
+            name='ai_race_motor_processing'
+        )
+    ])
